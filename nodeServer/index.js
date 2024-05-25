ff1fd485ae1b2f545cf9908ba7782a48d99ade87
Ashish2343 <a98126959@gmail.com>
2024-05-25 16:08:36 +0530

    Update CSS and Notification sound + bug fix

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -24,17 +24,22 @@ const users = {};
 io.on('connection', socket => {
     // Handle new user joined event
     socket.on('new-user-joined', name => {
-        console.log('New user joined:', name);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
 
     // Handle send message event
     socket.on('send', message => {
-        console.log('Message received:', message);
         socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
     });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    })
 });
+
+
 
 // Start the server
 server.listen(PORT, () => {
